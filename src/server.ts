94e4e71e5465f2d79565d6fdff7a7148024d91c7
Risa Yamamoto <yamari2422@gmail.com>
2024-11-27 19:25:17 -0800

    Finish assignment

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import productRouter from "./routes/product.routes";
 
 // Create server
 const app = express();
@@ -9,12 +10,20 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Routes
+app.use("/api/products", productRouter);
+
 // Connect to MongoDB and Start Server
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.DATABASE_URL!;
 mongoose
-  .connect("mongodb+srv://<db_user>:<db_password>@ciccc.o8yo3tc.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=<cluster_name>")
+  .connect(MONGO_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+    console.log("Connected to MongoDB database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}...`);
+    });
   })
-  .catch((err) => console.error('Failed to connect to MongoDB', err));
+  .catch((err) => {
+    console.error(`Failed to connect to database`);
+  });
